13fff65b63e764b8a20ada2d68ad348ea587fe8b
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2014-07-24 21:30:29 +0900

    initial commit

---
+++ src/DefinitionTableMaker/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace DefinitionTableMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.lstDatabases = new System.Windows.Forms.ListBox();
+			this.lstTables = new System.Windows.Forms.ListBox();
+			this.btnCreate = new System.Windows.Forms.Button();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lstDatabases
+			// 
+			this.lstDatabases.FormattingEnabled = true;
+			this.lstDatabases.ItemHeight = 20;
+			this.lstDatabases.Location = new System.Drawing.Point(8, 32);
+			this.lstDatabases.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.lstDatabases.Name = "lstDatabases";
+			this.lstDatabases.Size = new System.Drawing.Size(376, 104);
+			this.lstDatabases.TabIndex = 1;
+			this.lstDatabases.TabStop = false;
+			this.lstDatabases.Tag = "データベース";
+			this.lstDatabases.DoubleClick += new System.EventHandler(this.lstDatabases_DoubleClick);
+			// 
+			// lstTables
+			// 
+			this.lstTables.FormattingEnabled = true;
+			this.lstTables.ItemHeight = 20;
+			this.lstTables.Location = new System.Drawing.Point(8, 172);
+			this.lstTables.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.lstTables.Name = "lstTables";
+			this.lstTables.Size = new System.Drawing.Size(376, 104);
+			this.lstTables.TabIndex = 3;
+			this.lstTables.TabStop = false;
+			this.lstTables.Tag = "テーブル";
+			// 
+			// btnCreate
+			// 
+			this.btnCreate.Location = new System.Drawing.Point(184, 284);
+			this.btnCreate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnCreate.Name = "btnCreate";
+			this.btnCreate.Size = new System.Drawing.Size(100, 38);
+			this.btnCreate.TabIndex = 4;
+			this.btnCreate.Text = "定義書作成";
+			this.btnCreate.UseVisualStyleBackColor = true;
+			this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(8, 8);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(113, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "データベース一覧";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(8, 148);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(87, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "テーブル一覧";
+			// 
+			// btnExit
+			// 
+			this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnExit.Location = new System.Drawing.Point(288, 284);
+			this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(100, 38);
+			this.btnExit.TabIndex = 5;
+			this.btnExit.Text = "終了";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.btnCreate;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.CancelButton = this.btnExit;
+			this.ClientSize = new System.Drawing.Size(394, 352);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnCreate);
+			this.Controls.Add(this.lstTables);
+			this.Controls.Add(this.lstDatabases);
+			this.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "テーブル定義書出力";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.ListBox lstDatabases;
+        private System.Windows.Forms.ListBox lstTables;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ src/DefinitionTableMaker/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DefinitionTableMaker
+{
+    public partial class Form1 : Form
+    {
+        private string _dbName;
+
+        private readonly string ConnectString = @"Data Source=(local)\SQLEXPRESS; Integrated Security=SSPI;";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+			this._dbName = string.Empty;
+        }
+
+        
+		/// <summary>
+		/// Formロードイベント
+		/// </summary>
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				this.GetDatabases();
+			}
+			catch(SqlException ex)
+			{
+				MessageBox.Show(ex.Message, "アプリケーションエラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
+
+		/// <summary>
+		/// データベース一覧ダブルクリックイベント
+		/// </summary>
+		private void lstDatabases_DoubleClick(object sender, EventArgs e)
+		{
+			this.GetTables(this.lstDatabases.SelectedValue.ToString());
+		}
+
+		/// <summary>
+		/// 定義書作成クリックイベント
+		/// </summary>
+		private void btnCreate_Click(object sender, EventArgs e)
+		{
+			if (!this.InputCheck())
+			{
+				return;
+			}
+
+			var dt = new DataTable();
+
+			try
+			{
+				dt = this.GetColumnInfo(this.lstDatabases.SelectedValue.ToString(),
+										this.lstTables.SelectedValue.ToString());
+			}
+			catch(SqlException ex)
+			{
+				MessageBox.Show(ex.Message, "アプリケーションエラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+
+			string html = this.CreateHtml(dt);
+
+			try
+			{
+				this.SaveFile(html);
+			}
+			catch (IOException ex)
+			{
+				MessageBox.Show(ex.Message, "アプリケーションエラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
+
+		/// <summary>
+		/// リストボックス選択チェック
+		/// </summary>
+		/// <returns>True:正常 False:未選択</returns>
+		private bool InputCheck()
+		{
+			bool result = true;
+			var target = (from x in this.Controls.OfType<ListBox>()
+						  where x.SelectedValue == null
+						  select x).ToList();
+
+			if (0 < target.Count)
+			{
+				string s = string.Format("{0}を選択して下さい。", target[0].Tag);
+				MessageBox.Show(s, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				result = false; ;
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// HTMLデータ保存
+		/// </summary>
+		/// <param name="html">生成されたHTMLタグ</param>
+		private void SaveFile(string html)
+		{
+			this.saveFileDialog1.Filter = "HTMLファイル(*.html;*.htm)|*.html;*.htm";
+			
+			if (DialogResult.OK == this.saveFileDialog1.ShowDialog())
+			{
+				string s = Path.GetExtension(this.saveFileDialog1.FileName).Replace(".","");
+
+				if (s != "html" || s != "html")
+				{
+					this.saveFileDialog1.FileName = Path.ChangeExtension(this.saveFileDialog1.FileName, ".html");
+				}
+				using (var sw = new StreamWriter(this.saveFileDialog1.FileName, false, Encoding.GetEncoding("utf-8")))
+				{
+					try
+					{
+						sw.Write(html);
+						MessageBox.Show("できあがり～");
+
+						this.lstTables.DataSource = null;
+					}
+					finally
+					{
+						sw.Close();
+					}
+				}
+			}
+		}
+		/// <summary>
+		/// データベース一覧取得
+		/// </summary>
+        private void GetDatabases()
+        {
+			var dt = new DataTable();
+			string sql = "select name from sysdatabases";
+
+			try
+			{
+				dt = this.ExecuteCommand(sql, this.ConnectString);
+			}
+			catch
+			{
+				throw;
+			}
+
+            this.lstDatabases.ValueMember = "name";
+            this.lstDatabases.DisplayMember = "name";
+            this.lstDatabases.DataSource = dt;
+        }
+
+		/// <summary>
+		/// テーブル一覧取得
+		/// </summary>
+		/// <param name="dbName">DB名</param>
+        private void GetTables(string dbName)
+        {
+            string sql = "select t.name from sys.tables t order by t.name";
+            string connectString = string.Format("{0} Initial Catalog={1};", this.ConnectString, dbName);
+
+			var dt = new DataTable();
+
+			try
+			{
+				dt = this.ExecuteCommand(sql, connectString);
+			}
+			catch
+			{
+				throw;
+			}
+
+            this.lstTables.DisplayMember = "name";
+            this.lstTables.ValueMember = "name";
+            this.lstTables.DataSource = dt;
+        }
+
+		/// <summary>
+		/// テーブル定義情報取得
+		/// </summary>
+		/// <param name="dbName">DB名</param>
+		/// <param name="tableName">テーブル名</param>
+		/// <returns>取得結果@DataTable</returns>
+        private DataTable GetColumnInfo(string dbName, string tableName)
+        {
+            var sql = new StringBuilder();
+
+            sql.Length = 0;
+            sql.AppendLine("select");
+            sql.AppendLine("     c.ORDINAL_POSITION as No --");
+            sql.AppendLine("    ,c.COLUMN_NAME      as 列ID --物理ID");
+            sql.AppendLine("    ,c.DATA_TYPE        as データ型 --データ型");
+            sql.AppendLine("    ,case");
+            sql.AppendLine("       when c.CHARACTER_MAXIMUM_LENGTH is null then '-'");
+            sql.AppendLine("       when c.CHARACTER_MAXIMUM_LENGTH = '-1' then 'max'");
+            sql.AppendLine("       else convert(varchar,c.CHARACTER_MAXIMUM_LENGTH)");
+            sql.AppendLine("     end as バイト数");
+            sql.AppendLine("    ,case");
+            sql.AppendLine("       when c.NUMERIC_PRECISION is null then '-'");
+            sql.AppendLine("       else convert(varchar,c.NUMERIC_PRECISION)");
+            sql.AppendLine("     end as 桁数");
+            sql.AppendLine("    ,case");
+            sql.AppendLine("       when c.NUMERIC_SCALE is null then '-'");
+            sql.AppendLine("       else convert(varchar, c.NUMERIC_SCALE)");
+            sql.AppendLine("     end as 精度");
+            sql.AppendLine("    ,case");
+            sql.AppendLine("       when c.IS_NULLABLE = 'YES' then '○'");
+            sql.AppendLine("       else ''");
+            sql.AppendLine("     end as Null許容");
+            sql.AppendLine("from information_schema.columns c");
+            sql.AppendLine("left join (");
+            sql.AppendLine("            select");
+            sql.AppendLine("                 k.table_catalog");
+            sql.AppendLine("                ,k.table_schema");
+            sql.AppendLine("                ,k.table_name");
+            sql.AppendLine("                ,k.column_name");
+            sql.AppendLine("                ,t.constraint_type");
+            sql.AppendLine("            from information_schema.key_column_usage k");
+            sql.AppendLine("            inner join information_schema.table_constraints t");
+            sql.AppendLine("               on t.constraint_name = k.constraint_name");
+            sql.AppendLine("            where t.constraint_type = 'primary key'");
+            sql.AppendLine("               or t.constraint_type = 'unique'");
+            sql.AppendLine("          ) p");
+            sql.AppendLine(" on c.table_catalog = p.table_catalog");
+            sql.AppendLine("and c.table_schema  = p.table_schema");
+            sql.AppendLine("and c.table_name    = p.table_name");
+            sql.AppendLine("and c.column_name   = p.column_name");
+            sql.AppendFormat("where c.TABLE_NAME = '{0}'\n", tableName);
+            sql.AppendLine("order by");
+            sql.AppendLine("   c.ordinal_position");
+            
+            return this.ExecuteCommand(sql.ToString(), string.Format("{0} Initial Catalog={1};", this.ConnectString, dbName));
+        }
+
+		/// <summary>
+		/// HTMLタグ生成
+		/// </summary>
+		/// <param name="table">テーブル定義情報</param>
+		/// <returns>生成したHTMLタグ</returns>
+		private string CreateHtml(DataTable table)
+		{
+			var html = new StringBuilder();
+
+			html.Length = 0;
+			html.AppendLine("<!DOCTYPE html>");
+			html.AppendLine("<html>");
+			html.AppendLine("<head>");
+			html.AppendLine("  <meta charset=\"utf-8\">");
+			html.AppendFormat("  <title>{0}テーブル定義書</title>", this.lstTables.SelectedValue);
+			html.AppendLine("  <style type=\"text/css\">");
+			html.AppendLine("    table, th, td { border:1px #000000 solid; }");
+			html.AppendLine("  </style>");
+			html.AppendLine("</head>");
+			html.AppendLine("<body>");
+			html.AppendLine("  <div align=\"center\">");
+			html.AppendLine("  <table>");
+			html.AppendLine("  <tr>");
+			html.AppendLine("  <th>データベース名</th>");
+			html.AppendFormat("  <td>{0}</td>\n", this.lstDatabases.SelectedValue);
+			html.AppendLine("  </tr>");
+			html.AppendLine("  <tr>");
+			html.AppendLine("  <th>テーブル名</th>");
+			html.AppendFormat("  <td>{0}</td>\n", this.lstTables.SelectedValue);
+			html.AppendLine("  </tr>");
+			html.AppendLine("  </table></br>");
+			html.AppendLine("  <table>");
+			html.AppendLine("  <tr>");
+
+			//カラムヘッダ作成
+			foreach (DataColumn col in table.Columns)
+			{
+				html.AppendFormat("    <th>{0}</th>\n", col.Caption);
+			}
+
+			html.AppendLine("</tr>");
+
+			foreach (DataRow dr in table.Rows)
+			{
+				html.AppendLine("<tr>");
+
+				foreach (DataColumn col in table.Columns)
+				{
+					html.AppendFormat("    <td>{0}</td>\n", dr[col]);
+				}
+
+				html.AppendLine("</tr>");
+			}
+			html.AppendLine("</table>");
+			html.AppendLine("</div>");
+			html.AppendLine("</body>");
+			html.AppendLine("</html>");
+
+			return html.ToString();
+		}
+
+		/// <summary>
+		/// SQLコマンド実行
+		/// </summary>
+		/// <param name="sql">Select文</param>
+		/// <param name="connectString">接続文字列</param>
+		/// <returns>取得結果@DataTable</returns>
+        private DataTable ExecuteCommand(string sql, string connectString)
+        {
+            var dt = new DataTable();
+
+            using (var conn = new SqlConnection(connectString))
+            using (var comm = new SqlCommand(sql.ToString(), conn))
+            using (var adpt = new SqlDataAdapter(comm))
+            {
+				try
+				{
+					adpt.Fill(dt);
+				}
+				finally
+				{
+					conn.Close();
+				}
+            }
+
+            return dt;
+        }
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+			this.Dispose();
+
+			Application.Exit();
+		}
+    }
+}
