43b7d7e640a374e219654ac2a03ef9cb3856ac7d
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2014-08-02 14:46:41 +0900

    DBオブジェクト分割 レイアウト変更

---
+++ src/DefinitionTableMaker/DBCommander.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DefinitionTableMaker
+{
+	public class DBCommander
+	{
+		private readonly string ConnectString;
+
+		public DBCommander()
+		{
+			this.ConnectString = @"Data Source=(local)\SQLEXPRESS; Integrated Security=SSPI;";
+		}
+
+		public DBCommander(string dbName)
+		{
+			this.ConnectString = string.Format(@"Data Source=(local)\SQLEXPRESS; Integrated Security=SSPI; Initial Catalog={0};", dbName);
+		}
+
+		public DataTable ExecuteCommand(string sql, params object[] args)
+		{
+			var dt = new DataTable();
+
+			using (var conn = new SqlConnection(ConnectString))
+			using (var comm = new SqlCommand(sql, conn))
+			using (var adpt = new SqlDataAdapter(comm))
+			{
+				adpt.Fill(dt);
+			}
+			return dt;
+		}
+	}
+}
